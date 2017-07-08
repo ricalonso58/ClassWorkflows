070a9334880848025fd38ee6041f427d1ebd0507
Ric Alonso <ra58@me.com>
2017-07-08 15:21:33 -0700

    Crushed json file.

--- gulpfile.js
+++ gulpfile.js
@@ -7,6 +7,7 @@ var gulp = require('gulp'),
     gulpif = require('gulp-if'),
     uglify = require('gulp-uglify'),
     minifyHTML = require('gulp-minify-html'),
+    jsonminify = require('gulp-jsonminify'),
     concat = require('gulp-concat');
 
 
@@ -77,7 +78,7 @@ gulp.watch(coffeeSources, ['coffee']);
 gulp.watch(jsSources, ['js']);
 gulp.watch('components/sass/*.scss', ['compass']);
 gulp.watch('builds/development/*.html', ['html']);
-gulp.watch(jsonSources, ['json']);
+gulp.watch('builds/development/js/*.json', ['json']);
 
 });
 
@@ -96,7 +97,9 @@ gulp.task('html', function() {
 });
 
 gulp.task('json', function() {
-  gulp.src(outputDir + 'js/*.json')
+  gulp.src('builds/development/js/*.json')
+  .pipe(gulpif(env === 'production', jsonminify()))
+  .pipe(gulpif(env === 'production', gulp.dest('builds/production/js')))
   .pipe(connect.reload())
 });
 
