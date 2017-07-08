849e9f7dd6545c912f6f4b955de225b47fed95d3
Ric Alonso <ra58@me.com>
2017-07-08 12:32:16 -0700

    Added Browserify

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     coffee = require('gulp-coffee'),
+    browserify = require('gulp-browserify'),
     concat = require('gulp-concat');
 
     var coffeeSources = ['components/coffee/tagline.coffee'];
@@ -20,6 +21,7 @@ gulp.task('coffee', function() {
 
 gulp.task('js', function() {
   gulp.src(jsSources)
-    .pipe(concat('script.js'))
-    .pipe(gulp.dest('builds/development/js'))
+  .pipe(concat('script.js'))
+  .pipe(browserify())
+  .pipe(gulp.dest('builds/development/js'))
 });
